127d59e0228a27f999f989299f5a3bc9f447b1a9
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-07-10 12:29:39 -0300

    init: Set up project structure

---
+++ spring-crud-rest-api/src/test/java/com/jmfsoft/springrestapi/SpringCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmfsoft.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
